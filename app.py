e78d60d1d4b42cde252bf2b53926cae7dfe3516f
Minhne22 <90373167+Minhne22@users.noreply.github.com>
2025-02-03 19:24:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
-from flask_socketio import SocketIO, emit
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import datetime
 import requests
 import json
-from threading import Thread
+import threading
 import random
 import time
+import httpx
+import aiofiles
+import asyncio
+import os
+
+# if not os.path.exists('./uid.cache'):
+#     open('./uid.cache', 'w+').close()
 
 
 app = Flask(__name__)
 app.secret_key = "supersecretkey"
-socketio = SocketIO(app)
 
 # Kết nối MongoDB
 client = MongoClient("mongodb+srv://hahawelookcat:iXdyJYg1PSV140SZ@cluster0.my3yb.mongodb.net/")
@@ -27,206 +32,17 @@ token_collection = db["tokens"]
 
 
 # ==========================
-# Quản lý hàm
-# ==========================
-def get_comment_func(link_post, proxy={}, credential={}):
-    # Config Session
-    session = requests.Session()
-    ipport = f'{proxy["ip"]}:{proxy["port"]}' if not proxy['user'] else f'{proxy["user"]}:{proxy["password"]}@{proxy["ip"]}:{proxy["port"]}'
-    proxy = {
-        'http': f'http://{ipport}',
-        'https': f'http://{ipport}'
-    }
-    session.proxies = proxy
-    
-    headers = {
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-        'accept-language': 'en-US,en;q=0.9',
-        'cookie': 'datr=W06SZ_48TKb0XgBDj5NmAmV4; sb=W06SZweBwrhWi9P0gH85_X0b; dpr=1.5; wd=819x551',
-        'dpr': '1.5',
-        'priority': 'u=0, i',
-        'sec-ch-prefers-color-scheme': 'light',
-        'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132", "Google Chrome";v="132"',
-        'sec-ch-ua-full-version-list': '"Not A(Brand";v="8.0.0.0", "Chromium";v="132.0.6834.110", "Google Chrome";v="132.0.6834.110"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-model': '""',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-ch-ua-platform-version': '"19.0.0"',
-        'sec-fetch-dest': 'document',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-site': 'none',
-        'sec-fetch-user': '?1',
-        'upgrade-insecure-requests': '1',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
-        'viewport-width': '819',
-    }
-    
-    # cookies = {
-    #     'datr': 'W06SZ_48TKb0XgBDj5NmAmV4',
-    #     'sb': 'W06SZweBwrhWi9P0gH85_X0b',
-    #     'dpr': '1.5',
-    #     'wd': '819x551',
-    # }
-    
-    session.headers.update(headers)
-    # session.cookies.update(cookies)
-    try:
-        response = session.get(
-            link_post
-        ).text
-        
-        with open('./main.html', 'w+', encoding='utf8') as f:
-            f.write(response)
-        
-        post_id = response.split('"parent_feedback":{"id":"')[1].split('"')[0] if '"parent_feedback":{"id":"' in response \
-            else response.split("'parent_feedback': {'id': '")[1].split('\'')[0]
-        pid = response.split('"post_id":"')[1].split('"')[0] if '"post_id":"' in response\
-            else response.split("'post_id': '")[1].split('\'')[0]
-        
-        
-        lsd = response.split('"LSD",[],{"token":"')[1].split('"')[0] if '"LSD",[],{"token":"' in response else \
-                response.split("['LSD', [], {'token': '")[1].split('\'')[0]
-        
-        data = {
-            'av': '0',
-            '__aaid': '0',
-            '__user': '0',
-            '__a': '1',
-            '__req': 's',
-            '__hs': '',
-            'dpr': '1',
-            '__ccg': 'GOOD',
-            '__rev': '',
-            '__s': '',
-            '__hsi': '',
-            '__dyn': '',
-            '__csr': '',
-            '__comet_req': '15',
-            'lsd': lsd,
-            'jazoest': '2914',
-            '__spin_r': '',
-            '__spin_b': '',
-            '__spin_t': '',
-            'fb_api_caller_class': 'RelayModern',
-            'fb_api_req_friendly_name': 'CommentListComponentsRootQuery',
-            'variables': '{"commentsIntentToken":"RECENT_ACTIVITY_INTENT_V1","feedLocation":"TAHOE","feedbackSource":2,"focusCommentID":null,"scale":1,"useDefaultActor":false,"id":"' + post_id + '","__relay_internal__pv__IsWorkUserrelayprovider":false}',
-            'server_timestamps': 'true',
-            'doc_id': '8894656107282580',
-        }
-        
-        response = session.post(
-            'https://www.facebook.com/api/graphql/',
-            data=data
-        ).json()
-        
-        with open('./main.json', 'w+', encoding='utf8') as f:
-            f.write(json.dumps(response))
-        
-        comment = response['data']['node']['comment_rendering_instance_for_feed_location']['comments']['edges'][0]
-        info = comment['node']
-        try: 
-            data = {
-                'post_id': pid,
-                'text': info['body']['text'],
-                'name': info['author']['name'],
-                'time': info['comment_action_links'][0]['comment']['created_time'],
-                'author_id': info['discoverable_identity_badges_web'][0]['serialized'].split('actor_id')[1].split(':')[1].split(',')[0]
-            }
-        
-        except IndexError:
-            link_pro5 = info['author']['id']
-            print(link_pro5)
-            # Get ID Int
-            data = {
-                'link': f'https://www.facebook.com/{link_pro5}',
-            }
-
-            response = session.post('https://id.traodoisub.com/api.php', data=data)
-            print(response.text)
-            uid = response.json()['id']
-            
-            data = {
-                'post_id': pid,
-                'text': info['body']['text'],
-                'name': info['author']['name'],
-                'time': info['comment_action_links'][0]['comment']['created_time'],
-                'author_id': uid
-            }
-        
-        return data
-    
-    except IndexError:
-        post_id = response.split('"post_id":"')[1].split('"')[0] if '"post_id":"' in response\
-            else response.split("'post_id': '")[1].split('\'')[0]
-        if '"group_id":"' in response:
-            grid = response.split('"group_id":"')[1].split('"')[0] if '"group_id":"' in response\
-                else response.split("'group_id': '")[1].split('\'')[0]
-            pid = f'{grid}_{post_id}'
-        else:
-            pid = post_id
-        cookie, token = credential['cookie'], credential['token']
-        response = requests.get(
-            f'https://graph.facebook.com/{pid}',
-            params={
-                'access_token': token,
-                'order': 'reverse_chronological',
-                'fields': 'created_time,message,id,from'
-            },
-            headers={
-                'cookie': cookie
-            }
-        ).json()['data'][0]
-        
-        return {
-                'post_id': post_id,
-                'text': response['message'],
-                'name': response['from']['name'],
-                'time': response['created_time'].split('+')[0].replace('T', ' '),
-                'author_id': response['from']['id']
-            }
-        
-    except Exception as e:
-        with open('logs.txt', 'a+', encoding='utf8') as f:
-            now = datetime.datetime.now()
-            # Định dạng thời gian thành chuỗi
-            formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")
-            f.write(f'[{formatted_time}] - {e}\n')
-        return None
-
-def don_luong(link):
-    # return
-    while True:
-        print(link)
-        try:
-            data = get_comment_func(link, random.choice(proxies), random.choice(credentials))
-            print(data)
-            if data:
-                filter_condition = {"author_id": data["author_id"], "post_id": data["post_id"]}
-                comment_collection.update_one(filter_condition, {"$set": data}, upsert=True)
-        except Exception as e:
-            with open('logs.txt', 'a+', encoding='utf8') as f:
-                now = datetime.datetime.now()
-                # Định dạng thời gian thành chuỗi
-                formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")
-                f.write(f'[{formatted_time}] - {e}\n')
-        time.sleep(time_delay)
-                
-                
-        
-
-# ==========================
 # Quản lý Comment gần nhất (Trang chính)
 # ==========================
 @app.route("/")
 def comments():
-    comments = list(comment_collection.find().sort("time", -1).limit(10))
-    print(comments)
+    comments = list(comment_collection.find().sort("time", -1))
+    # print(comments)
     for comment in comments:
         # Định dạng thời gian (tùy chỉnh)
         comment["time"] = datetime.datetime.fromtimestamp(comment["time"]).strftime("%Y-%m-%d %H:%M:%S")
     return render_template("comments.html", comments=comments)
 
-from flask import jsonify
 
 @app.route("/comments/api", methods=["GET"])
 def get_comments_api():
@@ -243,7 +59,7 @@ def get_comments_api():
 def proxies():
     global proxies
     proxies = list(proxy_collection.find())
-    print(proxies)
+    # print(proxies)
     return render_template("proxies.html", proxies=proxies)
 
 @app.route("/proxies/add", methods=["GET", "POST"])
@@ -305,7 +121,7 @@ def add_link():
             flash("Vui lòng nhập URL bài viết.", "error")
             return redirect(url_for("add_link"))
         
-        Thread(target=don_luong, args=(url, )).start()
+        # Thread(target=don_luong, args=(url, )).start()
 
         link_collection.insert_one({"url": url.strip(), "description": description.strip()})
         flash("Đã thêm link bài viết thành công!", "success")
@@ -411,14 +227,11 @@ def fetch_token_from_cookie(cookie):
     Giả lập xử lý lấy token từ cookie. Thay thế logic này bằng thực tế.
     """
     
-    print(cookie)
     
     cookies = {
         x.split('=')[0]: x.split('=')[1]
         for x in cookie.replace(" ", "").split(';') if x
     }
-    
-    print(cookies)
         
     
     headers = {
@@ -443,19 +256,13 @@ def fetch_token_from_cookie(cookie):
     
     token = response.split('__accessToken="')[1].split('"')[0] if "__accessToken=" in response else None
     
-    print(token)
-    
     return token
 
-if __name__ == "__main__":
-    links = list(link_collection.find())
-    proxies = list(proxy_collection.find())
-    credentials = [x for x in list(token_collection.find()) if x['status'] == 'live']
+links = list(link_collection.find())
+proxies = list(proxy_collection.find())
+credentials = [x for x in list(token_collection.find()) if x['status'] == 'live']
 
-    time_delay = 5
-    
-    for url in links:
-        print(url)
-        Thread(target=don_luong, args=(url['url'], )).start()
-        time.sleep(2)
-    socketio.run(app, '0.0.0.0', 443)
+time_delay = 5
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', 443)
